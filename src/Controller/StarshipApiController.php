c458cc47a26079286837b346d71a3fcb1e419006
tab <tab@gmail.com>
2025-02-07 20:58:40 +0100

    add bestia

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -7,13 +7,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/starships/')]
 class StarshipApiController extends AbstractController
 {
-    #[Route('/api/starships')]
+    #[Route('', methods:['GET','POST'])]
     public function getCollection(StarshipRepository $repository): Response
     {
+
         $starships = $repository->findAll();
 
         return $this->json($starships);
+    }
+
+    #[Route('{id<\d+>}', methods:['GET','POST'])]
+    public function get(StarshipRepository $repository, int $id)
+    {
+        $starships = $repository->findAll();
+        return $this->json($starships[$id]);
     }
 }
